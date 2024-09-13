c37a452950a00713f20f24b753873941fad60368
CamiloRamirezR <c.ramirezr2@uniandes.edu.co>
2024-09-13 18:57:56 -0500

    logica de aerolinea

--- src/airline/airline.service.ts
+++ src/airline/airline.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AirlineEntity } from './airline.entity';
 import { Repository } from 'typeorm';
+import { BusinessError, BusinessLogicException } from '../shared/errors/business-errors';
+import { unknownMsg } from '../shared/utils/validation.utils';
 
 @Injectable()
 export class AirlineService {
@@ -10,4 +12,80 @@ export class AirlineService {
         @InjectRepository(AirlineEntity)
         private airlineRepository: Repository<AirlineEntity>,
     ) {}
+
+    async findAll(): Promise<AirlineEntity[]> {
+        return this.airlineRepository.find({
+            relations: ['airports'],
+        });
+    }
+
+    async findOne(id: string): Promise<AirlineEntity> {
+        const airline: AirlineEntity = await this.airlineRepository.findOne({
+            where: { id },
+            relations: ['airports'],
+        });
+
+        if (!airline) {
+            throw new BusinessLogicException(
+                unknownMsg('airline'),
+                BusinessError.NOT_FOUND,
+            );
+        }
+        
+        return airline;
+    }
+
+    async create(airline: AirlineEntity): Promise<AirlineEntity> {
+        const currentDate = new Date();
+
+        if (airline.foundationDate > currentDate) {
+            throw new BusinessLogicException(
+                'The foundation date cannot be in the future',
+                BusinessError.PRECONDITION_FAILED,
+            );
+        }
+
+        return this.airlineRepository.save(airline);
+    }
+
+    async update(id: string, airline: AirlineEntity): Promise<AirlineEntity> {
+        const existingAirline = await this.airlineRepository.findOne({ where: { id } });
+        const currentDate = new Date();
+
+        if (!existingAirline) {
+            throw new BusinessLogicException(
+                unknownMsg('airline'),
+                BusinessError.NOT_FOUND,
+            );
+        }
+
+        if (airline.foundationDate > currentDate) {
+            throw new BusinessLogicException(
+                'The foundation date cannot be in the future',
+                BusinessError.PRECONDITION_FAILED,
+            );
+        }
+
+        return await this.airlineRepository.save({
+            ...existingAirline,
+            ...airline,
+        });        
+    }
+
+    async delete(id: string){
+        const airline = await this.airlineRepository.findOne({ 
+            where: { id },
+            relations: ['airports'],
+        });
+
+        if (!airline) {
+            throw new BusinessLogicException(
+                unknownMsg('airline'),
+                BusinessError.NOT_FOUND,
+            );
+        }
+
+        return this.airlineRepository.remove(airline);
+    }
+
 }
