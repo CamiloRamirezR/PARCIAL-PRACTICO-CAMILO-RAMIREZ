c0cd1f2408396532f2303235f81e9b726c4a0055
CamiloRamirezR <c.ramirezr2@uniandes.edu.co>
2024-09-13 16:38:37 -0500

    entidad aeropuerto

--- src/airport/airport.entity.ts
+++ src/airport/airport.entity.ts
@@ -1,1 +1,20 @@
-export class AirportEntity {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class AirportEntity {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    code: string;
+
+    @Column()
+    city: string;
+
+    @Column()
+    country: string;
+}
