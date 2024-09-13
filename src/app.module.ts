7d6a9a1e69952a47edd09dd1e760cae86ee53802
CamiloRamirezR <c.ramirezr2@uniandes.edu.co>
2024-09-13 16:24:40 -0500

    se crea módulo de aerolínea

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AirlineModule } from './airline/airline.module';
 
 @Module({
-  imports: [],
+  imports: [AirlineModule],
   controllers: [AppController],
   providers: [AppService],
 })
