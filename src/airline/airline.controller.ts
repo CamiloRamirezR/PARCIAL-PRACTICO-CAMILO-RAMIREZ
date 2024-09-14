04f4f2e3623334725147ec0cb031d60b78c66564
CamiloRamirezR <c.ramirezr2@uniandes.edu.co>
2024-09-13 23:34:42 -0500

    Crear interceptor y DTO para aerolinea

--- src/airline/airline.controller.ts
+++ src/airline/airline.controller.ts
@@ -1,11 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseInterceptors } from '@nestjs/common';
 import { AirlineService } from './airline.service';
+import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors/business-errors.interceptor';
 
 @Controller('airlines')
+@UseInterceptors(BusinessErrorsInterceptor)
 export class AirlineController {
 
     constructor (
         private readonly airlineService: AirlineService,
     ) {}
-    
+
 }
