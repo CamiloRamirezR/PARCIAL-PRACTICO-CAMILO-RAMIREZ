469a64873d9bcbaa6478a1e3d75ce10ba8242ac1
CamiloRamirezR <c.ramirezr2@uniandes.edu.co>
2024-09-14 11:10:26 -0500

    Controlador de relación aerolinea-aeropuerto se ejecuta

--- src/airline-airport/airline-airport.module.ts
+++ src/airline-airport/airline-airport.module.ts
@@ -3,11 +3,13 @@ import { AirlineAirportService } from './airline-airport.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AirlineEntity } from '../airline/airline.entity';
 import { AirportEntity } from '../airport/airport.entity';
+import { AirlineAirportController } from './airline-airport.controller';
 
 @Module({
   providers: [AirlineAirportService],
   imports: [
     TypeOrmModule.forFeature([AirlineEntity, AirportEntity]),
   ],
+  controllers: [AirlineAirportController],
 })
 export class AirlineAirportModule {}
